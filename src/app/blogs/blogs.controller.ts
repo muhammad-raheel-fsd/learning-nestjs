a285f440f0961c967e7ddfdcc79350ed4726a4e9
Muhammad Raheel <raheeluafbrw@gmail.com>
2025-10-28 03:26:32 +0500

    feat: dynamic routes and query params

--- src/app/blogs/blogs.controller.ts
+++ src/app/blogs/blogs.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
@@ -13,11 +22,16 @@ export class BlogsController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Query() query: any) {
+    console.log('Slug:', query);
     return this.blogsService.findAll();
   }
-
+  // We can also add as much as params, but optional param should be at the end
+  // ? to make a param optional
+  // @Get(':id/:title?')
   @Get(':id')
+  // if we dont define which param to receive it will be an object containing all params - same for query params
+  // findOne(@Param() params)
   findOne(@Param('id') id: string) {
     return this.blogsService.findOne(+id);
   }
