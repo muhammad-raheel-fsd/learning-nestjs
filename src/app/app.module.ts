e0f46427e04030c0d6082c59f8f69153aae9ca88
Muhammad Raheel <raheeluafbrw@gmail.com>
2025-11-02 22:30:10 +0500

    feat: docker postgres config and sync db connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,28 @@ import { UsersModule } from './users/users.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, BlogsModule, ProductsModule, AuthModule],
+  imports: [
+    UsersModule,
+    BlogsModule,
+    ProductsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: process.env.POSTGRES_PORT
+        ? parseInt(process.env.POSTGRES_PORT, 10)
+        : 5432,
+      username: process.env.POSTGRES_USER || 'nestjs_app_user',
+      password: process.env.POSTGRES_PASSWORD || 'nestjs_app_password',
+      database: process.env.POSTGRES_DB || 'nestjs_app_db',
+      entities: [],
+      synchronize: true, // Note: set to false in production
+      entityPrefix: 'nestjs_app_',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
