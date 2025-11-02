273779bf4b98cd04646c880ab8e3d9c0186fa47e
Muhammad Raheel <raheeluafbrw@gmail.com>
2025-11-02 22:33:38 +0500

    feat: async db connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     BlogsModule,
     ProductsModule,
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: process.env.POSTGRES_PORT
-        ? parseInt(process.env.POSTGRES_PORT, 10)
-        : 5432,
-      username: process.env.POSTGRES_USER || 'nestjs_app_user',
-      password: process.env.POSTGRES_PASSWORD || 'nestjs_app_password',
-      database: process.env.POSTGRES_DB || 'nestjs_app_db',
-      entities: [],
-      synchronize: true, // Note: set to false in production
-      entityPrefix: 'nestjs_app_',
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: process.env.POSTGRES_PORT
+          ? parseInt(process.env.POSTGRES_PORT, 10)
+          : 5432,
+        username: process.env.POSTGRES_USER || 'nestjs_app_user',
+        password: process.env.POSTGRES_PASSWORD || 'nestjs_app_password',
+        database: process.env.POSTGRES_DB || 'nestjs_app_db',
+        entities: [],
+        synchronize: true, // Note: set to false in production
+        entityPrefix: 'nestjs_app_',
+      }),
     }),
   ],
   controllers: [AppController],
